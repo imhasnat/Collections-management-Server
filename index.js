31618d27a0b3cdaf4f81635b64282b3b4f9b728b
unknown <imhasnat00@gmail.com>
2024-08-26 19:25:03 +0600

    makig samsite none in the cookie

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post("/login", async (req, res) => {
     res.cookie("token", token, {
       // httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
