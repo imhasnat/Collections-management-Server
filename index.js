1ddc38115a2abec395b461a783cee1749cc67d97
unknown <imhasnat00@gmail.com>
2024-08-26 18:36:19 +0600

    cors issue

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.post("/login", async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
+      // httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
