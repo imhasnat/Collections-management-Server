1dc47d21b57baf0cfbfc69bb11d65aafd2653ae3
unknown <imhasnat00@gmail.com>
2024-08-26 18:49:16 +0600

    cors error checking

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       // httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      // secure: process.env.NODE_ENV === "production",
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
